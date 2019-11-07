1409d5d20402eeb7679f21bc2f0f5e7f7c4274ab
nachaphon <photitaen@gmail.com>
2019-11-07 19:46:06 +0700

    .

--- PJ_MRBS/MRBS/ADMIN/views.py
+++ PJ_MRBS/MRBS/ADMIN/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.
+def show_admin(request):
+    return HttpResponse("<h1>Show Admin<h1>")
--- PJ_MRBS/MRBS/USER/urls.py
+++ PJ_MRBS/MRBS/USER/urls.py
@@ -13,18 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+# from django.contrib import admin
 from django.urls import path
-from django.conf.urls import url , include
+from django.conf.urls import url
+from USER import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('',include('job_test.urls')),
+    path('page', views.show_user),
 
-    path('home_',include('HOME.urls')),
-    path('admin_',include('ADMIN.urls')),
-    path('user_',include('USER.urls')),
-    
-
-    
 ]

2f552aabc340d2fc44535314616bc8eafd9e1b13
nachaphon <photitaen@gmail.com>
2019-11-13 00:38:16 +0700

    .

---
+++ PJ_MRBS/MRBS/app_mrbs/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from app_mrbs import views
+urlpatterns = [
+    # path('/',views.index),
+]
--- PJ_MRBS/MRBS/app_mrbs/views.py
+++ PJ_MRBS/MRBS/app_mrbs/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+from django.template import loader
+from django.http import HttpResponse
 
 # Create your views here.

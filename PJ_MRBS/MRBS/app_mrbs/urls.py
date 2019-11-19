497a9402664d42745345f77994f41a1e449abb07
nachaphon <photitaen@gmail.com>
2019-11-20 01:51:44 +0700

    .

--- PJ_MRBS/MRBS/app_mrbs/urls.py
+++ PJ_MRBS/MRBS/app_mrbs/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from app_mrbs import views
+
 urlpatterns = [
-    # path('/',views.index),
+    path('',views.index),
 ]
--- PJ_MRBS/MRBS/app_mrbs/views.py
+++ PJ_MRBS/MRBS/app_mrbs/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.template import loader
 from django.http import HttpResponse
+from .templates import app_mrbs
 
 # Create your views here.
+
+
+
+def index(request):
+    return render(request,'app_mrbs/index.html')
+    # return HttpResponse("<h1>job<h1>")

d95fa9722e511f3ec3f44b760e28f44a55b425d3
nachaphon <photitaen@gmail.com>
2019-12-01 21:07:31 +0700

    .

--- PJ_MRBS/MRBS/app_mrbs/views.py
+++ PJ_MRBS/MRBS/app_mrbs/views.py
@@ -39,25 +39,69 @@ def admin(request):
     return HttpResponse("href=")
 
 def sort_room(request):
+    day = 1
     time = [12,30]
-    seat = 10
-    time_length = 3 # hr ไม่เกิน  4 hr
-    result = []
-    all_room = Timeslot.objects.all()
+    seat = 8
+    time_length_hr = 3 # hr ไม่เกิน  4 hr
+    time_length_min = 30 # min
+    if time_length_min == 30:
+        time_length_min = 1
+    slot_length = (time_length_hr*2) + time_length_min
+    all_room = Timeslot.objects.all()[(day*5)-5:(day*5)]
 
-    result = []
+    result_room = []
+    result_slot = []
+
+    all_slot = []
+    for i in all_room:
+        r1 = []
+        r1.append(i.status1)
+        r1.append(i.status2)
+        r1.append(i.status3)
+        r1.append(i.status4)
+        r1.append(i.status5)
+        r1.append(i.status6)
+        r1.append(i.status7)
+        r1.append(i.status8)
+        r1.append(i.status9)
+        r1.append(i.status10)
+        r1.append(i.status11)
+        r1.append(i.status12)
+        r1.append(i.status13)
+        r1.append(i.status14)
+        r1.append(i.status15)
+        r1.append(i.status16)
+        all_slot.append(r1)
+
 
   
-    # for room in all_room:
-    #     result1 = []
-    #     if int(room.room_seat) >= seat:
-    #         result1.append(room)
-    #         for slot in all_timeslot:
-    #             result1.append(slot[0])
-    #         result.append(result1)
+    for i in range(len(all_room)):
+        if int(all_room[i].roomday.room.room_seat) > seat:
+            count_slot_empty = 0
+            r_slot = []
+            for j in all_slot[i]:
+                if j == "empty":
+                    count_slot_empty += 1
+                    result_slot.append(j)
+                else:
+                    count_slot_empty = 0
+                    r_slot = []
+                if count_slot_empty >= slot_length:
+                    result_slot.append(r_slot)
+                    result_room.append(all_room[i])
+                    break
 
 
-    context = {'result':result , 'all_room':all_room }
+
+    context = {'result_room':result_room ,
+               'all_room':all_room ,
+               'slot_length':slot_length,
+               'seat':seat,
+               'len_result_room':len(result_room),
+               'result_slot':result_slot,
+
+
+            }
 
     return render(request, 'app_mrbs/sort.html', context)
 

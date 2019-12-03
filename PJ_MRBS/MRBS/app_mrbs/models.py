2110aa7d935c212718c89a4170a10f4a31f6878f
happymix555 <57962397+happymix555@users.noreply.github.com>
2019-12-03 17:11:07 +0700

    m

--- PJ_MRBS/MRBS/app_mrbs/models.py
+++ PJ_MRBS/MRBS/app_mrbs/models.py
@@ -78,3 +78,46 @@ class Timeslot(models.Model):
     user16 = models.CharField(max_length = 100, default = 'none')
     def __str__(self):
         return ("Timeslot_of_"+str(self.roomday))
+
+
+Seat = (
+    ('1', '1'),
+    ('2', '2'),
+    ('3', '3'),
+    ('4', '4'),
+    ('5', '5'),
+    ('6', '6'),
+    ('7', '7'),
+    ('8', '8'),
+    ('9', '9'),
+    ('10','10'),
+    ('11','11'),
+    ('12','12'),
+    ('13','13'),
+    ('14','14'),
+    ('15','15'),
+    ('16','16'),
+    ('17','17'),
+    ('18','18'),
+    ('19','19'),
+    ('20','20'),
+    ('21','21'),
+    ('22','22'),
+    ('23','23'),
+    ('24','24'),
+    ('25','25'),
+)
+Period = (
+    ('1', '0.5'),
+    ('2', '1'),
+    ('3', '1.5'),
+    ('4', '2'),
+    ('5', '2.5'),
+    ('6', '3'),
+    ('7', '3.5'),
+    ('8', '4'),
+)
+
+class Sortmodel(models.Model):
+    seat    = models.CharField(max_length=3, choices=Seat)
+    period   = models.CharField(max_length=3, choices=Period)
--- PJ_MRBS/MRBS/app_mrbs/urls.py
+++ PJ_MRBS/MRBS/app_mrbs/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     path('user_page/',views.admin , name= 'user-page'),
     path('account/',views.check_account ,name='ck'),
     path('sort/',views.sort_room ,name='sort'),
-    
+    path('sortmix/',views.sort_mix ,name='sortmix'),
     path('user/pick_day',views.pick_day, name = "pick_day"),
     path('user/pick_day/pick_room/<int:day_id>',views.pick_room, name = "pick_room"),
     path('base0',views.homepage2, name = "base0"),
